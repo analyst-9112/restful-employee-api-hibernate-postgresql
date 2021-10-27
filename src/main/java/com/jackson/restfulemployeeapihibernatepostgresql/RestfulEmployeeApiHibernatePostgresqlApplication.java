3084290e8742d914e879c8eb8a1d02347cc35cfa
Jackson II <gjacks998@cable.comcast.com>
2021-10-27 16:37:22 -0400

    Initial commit

---
+++ src/main/java/com/jackson/restfulemployeeapihibernatepostgresql/RestfulEmployeeApiHibernatePostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.restfulemployeeapihibernatepostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulEmployeeApiHibernatePostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulEmployeeApiHibernatePostgresqlApplication.class, args);
+	}
+
+}
